6b42750df26e0da44f8ef6845ec4103b9f9a1a70
David Brownhill <davidbrownhill70@btinternet.com>
2021-04-16 14:16:55 +0100

    First commit

--- src/test/java/com/cc/practicaltest/steps/BranchSearchSteps.java
+++ src/test/java/com/cc/practicaltest/steps/BranchSearchSteps.java
@@ -11,8 +11,6 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.support.ui.ExpectedCondition;
-import org.openqa.selenium.support.ui.WebDriverWait;
 
 import static junit.framework.TestCase.assertTrue;
 
